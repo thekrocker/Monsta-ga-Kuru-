4f9b42a0fac3f34dab19a4c516aa27b92462b274
thekrocker <sdedeakay@yahoo.com>
2021-08-13 19:05:13 +0300

    Player created and instantiated on scene

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+
+    [SerializeField] private Player playerPrefab;
+    [SerializeField] private Transform playerSpawnPoint;
+
+
+    private void Start()
+    {
+
+        
+        Player player = Instantiate(playerPrefab, playerSpawnPoint.position, Quaternion.identity);
+        player.GetComponent<Player>();
+        
+        
+
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public int range;
+    private void Start()
+    {
+        
+    }
+}
