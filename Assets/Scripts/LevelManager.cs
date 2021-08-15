577d5a17b2b9d8655ea9534a187012c424552181
thekrocker <sdedeakay@yahoo.com>
2021-08-16 01:20:43 +0300

    added LevelManager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public enum LevelState
+    {
+        Level1,
+        Level2,
+        Level3,
+        Level4,
+        Level5
+    }
+
+    public LevelState levelstate;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        switch (levelstate)
+        {
+            case LevelState.Level1:
+                Debug.Log("It's level 1.");
+                break;
+            
+            case LevelState.Level2:
+                // Do things.
+                Debug.Log("It's level 2.");
+                break;
+            
+            case LevelState.Level3:
+                // Do things.
+                break;
+            
+            case LevelState.Level4:
+                // Do things.
+                break;
+            
+            case LevelState.Level5:
+                // Do things.
+                break;
+            
+            
+        }
+        
+    }
+
+}
+
